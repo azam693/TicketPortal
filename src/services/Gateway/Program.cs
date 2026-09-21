784304937c8a1fc95df20da7cd85f998081cbe6d
azam693 <azamat.talhin@mail.ru>
2026-09-21 14:15:45 +0200

    feat: Add Scalar for Gateway

--- src/services/Gateway/Program.cs
+++ src/services/Gateway/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
@@ -5,6 +7,15 @@ builder.Services
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+app.MapScalarApiReference(options =>
+{
+    options
+        .AddDocument("catalog", "Catalog", "/openapi/catalog.json")
+        .AddDocument("booking", "Booking", "/openapi/booking.json")
+        .AddDocument("order", "Order", "/openapi/order.json")
+        .AddDocument("payment", "Payment", "/openapi/payment.json");
+});
 
 app.MapReverseProxy();
 
